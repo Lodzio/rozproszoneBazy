3be80f5be790fecce28e122e715e52393c3828f6
czachadymi <47981406+czachadymi@users.noreply.github.com>
2020-11-16 00:12:22 +0100

    empty

--- init.sql
+++ init.sql
@@ -1,3 +1,9 @@
 CREATE PUBLIC DATABASE LINK database1 CONNECT TO system IDENTIFIED BY oracle USING 'database1';
 CREATE PUBLIC DATABASE LINK database2 CONNECT TO system IDENTIFIED BY oracle USING 'database2';
 CREATE PUBLIC DATABASE LINK database3 CONNECT TO system IDENTIFIED BY oracle USING 'database3';
+
+CREATE TABLE SYSTEM.booking
+( customer_id number(10) NOT NULL,
+  customer_name varchar2(50) NOT NULL,
+  city varchar2(50)
+);
