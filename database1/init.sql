9d45382085d54e21a80a63c5384b7fa6c8dd0dc0
czachadymi <47981406+czachadymi@users.noreply.github.com>
2020-11-16 19:10:26 +0100

    empty

--- database1/init.sql
+++ database1/init.sql
@@ -2,31 +2,18 @@ CREATE TABLE SYSTEM.Sklep
 ( id number(10) NOT NULL,
   ulica varchar2(50) NOT NULL,
   Miasto varchar2(50) NOT NULL,
-  NrLokalu number(5) NOT NULL,
+  NrLokalu number(5),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE SYSTEM.MieszankaZiołowa
+CREATE TABLE SYSTEM.MieszankaZiolowa
 ( id number(10) NOT NULL,
   Nazwa varchar2(50) NOT NULL,
-  Sklep varchar2(50) NOT NULL,
-  Dostępność boolean,
-  Cena number(5) NOT NULL,
+  Sklep number(10) NOT NULL,
+  Dostepnosc number(1),
+  Cena number(5),
   PRIMARY KEY (id),
-  FOREIGN KEY (Sklep) references Sklep(id)
-);
-
-CREATE TABLE SYSTEM.Zakup
-( id number(10) NOT NULL,
-  Sklep varchar2(50) NOT NULL,
-  Mieszanka varchar2(50) NOT NULL,
-  Nabywca varchar2(50) NOT NULL,
-  Datazakupu date NOT NULL,
-  Zakupaabonamentu boolean,
-  Rodzajabonamentu varchar2(25),
-  PRIMARY KEY (id),
-  FOREIGN KEY (Sklep) references Sklep(id),
-  FOREIGN KEY (Nabywca) references Nabywca(id),
-  FOREIGN KEY (Rodzajabonamentu) references Abonament(id),
+  FOREIGN KEY (Sklep) references SYSTEM.Sklep(id)
 );
 
 CREATE TABLE SYSTEM.Nabywca
@@ -36,7 +23,7 @@ CREATE TABLE SYSTEM.Nabywca
     Nazwisko Varchar2(20) NOT NULL,
     Numerkonta number(20) NOT NULL,
     Numertelefonu number (11) NOT NULL,
-    Adresemail varchar2(20),
+    Adresemail varchar2(20)
 )
 
 CREATE TABLE SYSTEM.Abonament
@@ -44,5 +31,20 @@ CREATE TABLE SYSTEM.Abonament
     id number(5) NOT NULL,
     Nazwa varchar2(20) NOT NULL,
     Okrestrwania Varchar2(20) NOT NULL,
-    Cena number(20) NOT NULL,
+    Cena number(20) NOT NULL
 )
+
+CREATE TABLE SYSTEM.Zakup
+( id number(10) NOT NULL,
+  Sklep varchar2(50) NOT NULL,
+  Mieszanka varchar2(50) NOT NULL,
+  Nabywca number(10) NOT NULL,
+  Datazakupu date,
+  Zakupaabonamentu number(1),
+  Rodzajabonamentu number(10),
+  PRIMARY KEY (id),
+  FOREIGN KEY (Sklep) references Sklep(id),
+  FOREIGN KEY (Nabywca) references Nabywca(id),
+  FOREIGN KEY (Rodzajabonamentu) references Abonament(id)
+);
+
