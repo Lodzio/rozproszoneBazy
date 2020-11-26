59194d132518798ac3f75827f14bef037f6b60e3
czachadymi <47981406+czachadymi@users.noreply.github.com>
2020-11-26 21:59:57 +0100

    'empty'

--- database1/init.sql
+++ database1/init.sql
@@ -40,9 +40,9 @@ CREATE TABLE Administrator1.Nabywca
     id number(5) NOT NULL,
     Imie varchar2(20) NOT NULL,
     Nazwisko Varchar2(20) NOT NULL,
-    Numerkonta number(20) NOT NULL,
+    Numerkonta number(38) NOT NULL,
     Numertelefonu number (11) NOT NULL,
-    Adresemail varchar2(20),
+    Adresemail varchar2(23),
     PRIMARY KEY (id)
 );
 
@@ -55,10 +55,20 @@ CREATE TABLE Administrator1.Abonament
     PRIMARY KEY (id)
 );
 
+CREATE TABLE Administrator1.MieszankaZiolowa
+( id number(10) NOT NULL,
+  Nazwa varchar2(50) NOT NULL,
+  Sklep number(10) NOT NULL,
+  Dostepnosc number(4),
+  Cena number(5),
+  PRIMARY KEY (id),
+  FOREIGN KEY (Sklep) references Administrator1.Sklep(id)
+);
+
 CREATE TABLE Administrator1.Zakup
 ( id number(10) NOT NULL,
   Sklep number(5),
-  Mieszanka varchar2(50) NOT NULL,
+  Mieszanka number(10) NOT NULL,
   Nabywca number(10) NOT NULL,
   Datazakupu date,
   Zakupaabonamentu number(1),
@@ -66,28 +76,60 @@ CREATE TABLE Administrator1.Zakup
   PRIMARY KEY (id),
   FOREIGN KEY (Sklep) REFERENCES Administrator1.Sklep(id),
   FOREIGN KEY (Nabywca) REFERENCES Administrator1.Nabywca(id),
-  FOREIGN KEY (Rodzajabonamentu) REFERENCES Administrator1.Abonament(id)
+  FOREIGN KEY (Rodzajabonamentu) REFERENCES Administrator1.Abonament(id),
+  FOREIGN KEY (Mieszanka) REFERENCES Administrator1.MieszankaZiolowa(id)
 );
 
-CREATE TABLE Administrator1.MieszankaZiolowa
-( id number(10) NOT NULL,
-  Nazwa varchar2(50) NOT NULL,
-  Sklep number(10) NOT NULL,
-  Dostepnosc number(3),
-  Cena number(5),
-  PRIMARY KEY (id),
-  FOREIGN KEY (Sklep) references Administrator1.Sklep(id)
-);
 
-INSERT INTO Administrator1.Sklep(id,ulica,Miasto,NrLokalu) Values('63','Malinowa','Wroclaw','51');
-INSERT INTO Administrator1.MieszankaZiolowa(id,Nazwa,Sklep,Dostepnosc,Cena) Values('961','Rozgrzejsie','63','1','29');
 
-UPDATE Administrator1.Sklep
-    SET Miasto = 'NEW YORK' 
-    WHERE id = 1; 
-UPDATE Administrator1.MieszankaZiolowa
-    SET Dostepnosc = 0
-    WHERE id = 1; 
+INSERT INTO Administrator1.Sklep(id,ulica,Miasto,NrLokalu) 
+Values('1','Malinowa','Wroclaw','4');
+
+INSERT INTO Administrator1.Nabywca(id,Imie,Nazwisko,Numerkonta,Numertelefonu, Adresemail)
+Values('1','Mateusz','Ziaja','11114015601081110181488249','111222333','M.Ziaja@gmail.com');
+INSERT INTO Administrator1.Nabywca(id,Imie,Nazwisko,Numerkonta,Numertelefonu, Adresemail)
+Values('2','Dariusz','Talik','21114015601081110181488249','211222333','D.Talik@gmail.com');
+INSERT INTO Administrator1.Nabywca(id,Imie,Nazwisko,Numerkonta,Numertelefonu, Adresemail)
+Values('3','Mariusz','Twardowski','31114015601081110181488249','411222333','M.Twardowski@gmail.com');
+INSERT INTO Administrator1.Nabywca(id,Imie,Nazwisko,Numerkonta,Numertelefonu, Adresemail)
+Values('4','Tomasz','Babka','41114015601081110181488249','511222333','T.babka@gmail.com');
+INSERT INTO Administrator1.Nabywca(id,Imie,Nazwisko,Numerkonta,Numertelefonu, Adresemail)
+Values('5','Robert','Droga','51114015601081110181488249','611222333','R.droga@gmail.com');
+
+INSERT INTO Administrator1.Abonament(id,Nazwa,Okrestrwania,Cena)
+Values('1','Premium Begginer','7','60');
+INSERT INTO Administrator1.Abonament(id,Nazwa,Okrestrwania,Cena)
+Values('2','Premium Intermediate','7','160');
+INSERT INTO Administrator1.Abonament(id,Nazwa,Okrestrwania,Cena)
+Values('3','Premium Proffesional','7','260');
+
+
+INSERT INTO Administrator1.MieszankaZiolowa(id,Nazwa,Sklep,Dostepnosc,Cena)
+Values('1','Rozgrzejsie','1','100','30');
+INSERT INTO Administrator1.MieszankaZiolowa(id,Nazwa,Sklep,Dostepnosc,Cena)
+Values('2','PoprawOdpornosc','1','100','30');
+INSERT INTO Administrator1.MieszankaZiolowa(id,Nazwa,Sklep,Dostepnosc,Cena)
+Values('3','Swiateczna','1','75','25');
+INSERT INTO Administrator1.MieszankaZiolowa(id,Nazwa,Sklep,Dostepnosc,Cena)
+Values('4','NaKatar','1','75','25');
+INSERT INTO Administrator1.MieszankaZiolowa(id,Nazwa,Sklep,Dostepnosc,Cena)
+Values('5','Orzezwijsie','1','50','35');
+INSERT INTO Administrator1.MieszankaZiolowa(id,Nazwa,Sklep,Dostepnosc,Cena)
+Values('6','ZastrzykEnergii','1','50','35');
+
+INSERT INTO Administrator1.Zakup(id,Sklep,Mieszanka,Nabywca,Datazakupu,Zakupaabonamentu,Rodzajabonamentu)
+Values('1','1','1','3','14-APR-2020','1','1');
+INSERT INTO Administrator1.Zakup(id,Sklep,Mieszanka,Nabywca,Datazakupu,Zakupaabonamentu,Rodzajabonamentu)
+Values('2','1','3','4','15-APR-2020','1','2');
+INSERT INTO Administrator1.Zakup(id,Sklep,Mieszanka,Nabywca,Datazakupu,Zakupaabonamentu,Rodzajabonamentu)
+Values('3','1','6','1','16-APR-2020','1','3');
+
+-- UPDATE Administrator1.Sklep
+--     SET Miasto = 'NEW YORK' 
+--     WHERE id = 1; 
+-- UPDATE Administrator1.MieszankaZiolowa
+--     SET Dostepnosc = 0
+--     WHERE id = 1; 
 
 GRANT CREATE MATERIALIZED VIEW TO Administrator1;
 GRANT CREATE DATABASE LINK TO Administrator1;
